dfd0b3c8b89160b5085d4674150d36e877f8f3bc
M G O Vishwajith <72697298+oshan-vishwajith@users.noreply.github.com>
2024-09-01 15:05:35 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./footer.css">
+</head>
+
+<body>
+
+    <div id="custom-footer">
+        <footer id="custom-footer" class="text-center text-white">
+            <div class="container">
+                <section class="mt-2">
+                    <div class="row text-center d-flex justify-content-center pt-5">
+                        <div class="col-md-2">
+                            <h6 class="text-uppercase font-weight-bold">
+                                <a href="index.php" class="">Home</a>
+                            </h6>
+                        </div>
+                        <div class="col-md-2">
+                            <h6 class="text-uppercase font-weight-bold">
+                                <a href="New_arrival.php" class="text-white">New Arrivals</a>
+                            </h6>
+                        </div>
+                        <div class="col-md-2">
+                            <h6 class="text-uppercase font-weight-bold">
+                                <a href="Best_seller.php" class="text-white">Best Seller</a>
+                            </h6>
+                        </div>
+                        <div class="col-md-2">
+                            <h6 class="text-uppercase font-weight-bold">
+                                <a href="aboutus.php" class="text-white">About Us</a>
+                            </h6>
+                        </div>
+                        <div class="col-md-2">
+                            <h6 class="text-uppercase font-weight-bold">
+                                <a href="contact.php" class="text-white">Contact Us</a>
+                            </h6>
+                        </div>
+                    </div>
+                </section>
+
+                <hr class="my-2" />
+
+                <section class="mb-5">
+                    <div class="row d-flex justify-content-center">
+                        <div class="col-lg-8">
+                            <p>
+                                Explore the exquisite artistry and meticulous craftsmanship that define our unique leather products.
+                                Our unwavering passion for quality and our keen attention to detail are evident in every piece we create.
+                                Each item is a testament to our dedication and skill, reflecting the care and precision that go into our work.
+                                Dive deeper into our story and discover the core values that inspire and drive us every day.
+                            </p>
+                        </div>
+                    </div>
+                </section>
+
+                <div id="social-links" section class="text-center mb-5">
+
+                    <a href="https://www.facebook.com" class="text-white me-4" style="text-decoration: none; color: inherit;">
+                        <i class="fa-brands fa-facebook"></i>
+                    </a>
+
+                    <a href="https://www.twitter.com" class="text-white me-4" style="text-decoration: none; color: inherit;">
+                        <i class="fa-brands fa-twitter"></i>
+                    </a>
+                    <a href="https://www.google.com" class="text-white me-4" style="text-decoration: none; color: inherit;">
+                        <i class="fa-brands fa-google"></i>
+                    </a>
+                    <a href="https://www.instagram.com" class="text-white me-4" style="text-decoration: none; color: inherit;">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                    <a href="https://www.linkedin.com" class="text-white me-4" style="text-decoration: none; color: inherit;">
+                        <i class="fa-brands fa-linkedin"></i>
+                    </a>
+                    <a href="https://www.github.com" class="text-white me-4" style="text-decoration: none; color: inherit;">
+                        <i class="fa-brands fa-github"></i>
+                    </a>
+                    </section>
+                </div>
+            </div>
+
+            <div class="text-center p-3" id="footer-copyright">
+                © 2024 Copyright:
+                <a class="text-white" href="index.php">Serandip Leathers</a>
+            </div>
+        </footer>
+    </div>
+
+</body>
+
+</html>
